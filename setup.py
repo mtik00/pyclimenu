611690634c921889a6277ced3be7aa749f8dbe4f
Timothy McFadden <tim@timandjamie.com>
2017-04-16 11:49:30 -0600

    removing commented code from setup

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@
 '''
 climenu setup script
 '''
-# import os
 from setuptools import setup
 
 
 MAIN_NS = {}
-VER_PATH = 'climenu.py'  # os.path.join('climenu', 'version.py')
+VER_PATH = 'climenu.py'
 
 
 if __name__ == '__main__':
     execfile(VER_PATH, MAIN_NS)
-    # with open(VER_PATH) as ver_file:
-    #     exec(ver_file.read(), MAIN_NS)  # pylint: disable=W0122
-
     DESC = open('README.rst').read()
 
     setup(
@@ -45,8 +41,6 @@ if __name__ == '__main__':
         author_email="tim@timandjamie.com",
         license='MIT',
         install_requires=[],
-        # packages=find_packages(),
-        # package_data={"climenu": ['.*']},
         zip_safe=True,
         include_package_data=True,
         long_description=DESC
