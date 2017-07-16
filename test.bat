bf5e018f5eb09a2c37d2c9b1491c36e4380598a7
Timothy McFadden <tim@timandjamie.com>
2017-07-15 18:58:50 -0600

    changing coverage to report only module

--- test.bat
+++ test.bat
@@ -1,1 +1,4 @@
-coverage run -p -m py.test tests/test1 && coverage run -p -m py.test tests/test_nested && coverage combine && coverage report -m
+coverage run -p -m py.test tests/test1
+coverage run -p -m py.test tests/test_nested
+coverage combine
+coverage report -m --include=climenu.py
