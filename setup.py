3f2bcddc9c5d0fdb246aa7dda6182dfb4bed3d62
Timothy McFadden <tim@timandjamie.com>
2017-07-16 16:56:37 -0600

    specifying python versions in setup.py trove classifiers

--- setup.py
+++ setup.py
@@ -43,7 +43,13 @@ if __name__ == '__main__':
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX :: Linux',
             'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Development Status :: 5 - Production/Stable'
         ],
 
