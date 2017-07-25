c83dafbce9f5ad56cb6b3790088993ad5dc4421b
Timothy McFadden <timothy.mcfadden@broadcom.com>
2017-07-25 10:50:43 -0600

    INTERNAL: Add `docs/conf.py` to version rev

--- NOTES.md
+++ NOTES.md
@@ -16,7 +16,8 @@ To manage the project, you'll need to install the project's requirements file:
 
 ## Releasing
 *   Make sure everything is checked in and pushed
-*   Change the version `climenu.__version__`
+*   Change the version using `fab.rev`  
+    That will change both `climenu.__version__` and `docs/conf.py`
 *   Make sure `release-notes.md` is up to date
 *   Add/commit/push with `prep for vX.Y.Z release`
 *   Create a tag:  
--- fabfile/ver.py
+++ fabfile/ver.py
@@ -6,8 +6,9 @@ This module manages the version info.
 
 # Imports #####################################################################
 import re
-# from fabric.api import task
-from .constants import VER_FILE
+from fabric.api import task
+from .constants import VER_FILE, DOCS_CONF_FILE
+from .helpers import true
 
 
 # Metadata ####################################################################
@@ -29,3 +30,32 @@ def get_version():
         raise Exception("Could not get version")
 
     return match.group('version')
+
+
+@task
+def rev():
+    """Increases the 'minor' version number by 1"""
+    text = open(VER_FILE).read()
+    whole, ver = re.search("^(__version__\s+=\s+['\"](.*?)['\"]\s*)$", text, re.MULTILINE).groups()
+    major, minor, patch = ver.split('.', 2)
+    new_minor = str(int(minor) + 1)
+
+    # NOTE: When reving minor, it's customary to set patch to "0"
+    new_ver = ".".join([major, new_minor, "0"])
+
+    print("Old version number is [%s]" % ver)
+    print("New version number is [%s]" % new_ver)
+    val = raw_input("OK [Yn]? ")
+    while not true(val):
+        new_ver = raw_input("Enter new version: ")
+        print("Using new version [%s]" % new_ver)
+        val = raw_input("OK [Yn]? ")
+
+    text = text.replace(ver, new_ver)
+    with open(VER_FILE, "wb") as fh:
+        fh.write(text)
+
+    text = open(DOCS_CONF_FILE).read()
+    text = re.sub("(version|release) = u'.*?'", r"\1 = u'%s'" % new_ver, text)
+    with open(DOCS_CONF_FILE, 'wb') as fh:
+        fh.write(text)
